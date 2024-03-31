ebd39c42c4353a0392ba3ca7200279489d6b5a60
Curtis Berry <curtis2428@gmail.com>
2024-03-30 18:18:50 -0700

    huh

--- GlamByManpreet/src/Pages/Home.js
+++ GlamByManpreet/src/Pages/Home.js
@@ -7,7 +7,7 @@ function HomePage() {
         <div>
             <h1>Welcome to the Home Page!</h1>
             <p>This is the content of the home page. Congratulations you have your environment set up</p>
-            <p>Ruben LOVES the 49ers!!!!</p>
+            <p>Ruben LOVES the 49ers!!!</p>
             <img src={catImage} alt="Logo" />
 
             <div>
