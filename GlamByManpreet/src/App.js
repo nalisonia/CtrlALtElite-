77df7ab6807e9f57f4c5184d32faf9cc3b6f00de
Ruben <ruben@rubens-laptop.attlocal.net>
2024-03-31 20:41:47 -0700

    Worked on header

--- GlamByManpreet/src/App.js
+++ GlamByManpreet/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import HomePage from './Pages/Home.js';
+import Header from './Components/header.js';
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <HomePage />
+            
         </div>
     );
 }
