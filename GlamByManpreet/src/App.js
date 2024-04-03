736ee4a5bf12de906a8264f00c1399b85139be01
Ruben <ruben000_2@live.com>
2024-04-02 19:55:24 -0700

    Added pages to all links and added a link to home page from the title

--- GlamByManpreet/src/App.js
+++ GlamByManpreet/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import HomePage from './Pages/Home.js';
-import Header from './Components/header.js';
+import Header from './Components/navbar.js';
 import AboutMe from './Pages/AboutMe.js'; // Import the AboutMe component
+import Services from './Pages/Services.js';
+import BookingInqury from './Pages/BookingInqury.js';
+import ContactMe from './Pages/ContactMe.js';
+import Gallery from './Pages/Gallery.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and BrowserRouter
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
                 <Routes> {/* Wrap your routes with Routes */}
                     <Route path="/" element={<HomePage />} /> {/* Use element prop */}
                     <Route path="/AboutMe" element={<AboutMe />} /> {/* Use element prop */}
+                    <Route path="/Services" element={<Services/>} /> {/* Use element prop */}
+                    <Route path="/BookingInqury" element={<BookingInqury/>} /> {/* Use element prop */}
+                    <Route path="/ContactMe" element={<ContactMe/>} /> {/* Use element prop */}
+                    <Route path="/Gallery" element={<Gallery/>} /> {/* Use element prop */}
                 </Routes>
             </div>
         </Router>
--- GlamByManpreet/src/Components/navbar.js
+++ GlamByManpreet/src/Components/navbar.js
@@ -12,6 +12,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import ticktokLogo from '../assets/images/tictokicon.png';
 import IGlogo from '../assets/images/Instagram_icon.png';
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import '../Styles/NavBar.css'; // Import CSS file for styling 
+
 
 
 
@@ -19,17 +21,15 @@ export default function Header() {
 	return (
 		<div className="header-container">
 <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+	
     <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '10px', paddingBottom: '10px' }}>
         {/* Container for IconButton components */}
 
-
-		<Typography
-    		variant="h6"
-    		component="div"
-    		sx={{ color: 'black', fontWeight: 'bold', marginRight: 'auto', marginLeft: 'auto' }} // Centering Typography component
-		>
-    		GlamByManpreet
+		<Typography variant="h6" component="div" sx={{ color: 'black', fontWeight: 'bold', marginRight: 'auto', marginLeft: 'auto' }}>
+    		<Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>GlamByManpreet</Link>
 		</Typography>
+	
+
 
         <div style={{ display: 'flex', flexDirection: 'column' }}>
 
@@ -49,9 +49,12 @@ export default function Header() {
     </Toolbar>
 	<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingBottom: '10px' }}>
     <Link to="/AboutMe" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>About</Link>
-    <Link to="/" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>Home</Link>
-    <Link to="/" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>Home</Link>
-    <Link to="/" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>Home</Link>
+    <Link to="/Services" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>Services</Link>
+	<Link to="/BookingInqury" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>BookingInqury</Link>
+	<Link to="ContactMe" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>ContactMe</Link>
+	<Link to="/Gallery" style={{ textDecoration: 'none', color: 'black', marginRight: '10px' }}>Gallery</Link>
+	
+
 </div>
 
 </AppBar>
