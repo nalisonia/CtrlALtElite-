0f40c351b06bbffeb43c6a8c8f612de2b2747737
Ruben <ruben000_2@live.com>
2024-04-02 18:46:54 -0700

    Worked more on main

--- GlamByManpreet/src/Pages/AboutMe.js
+++ GlamByManpreet/src/Pages/AboutMe.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../Styles/AboutMe.css'; // Import CSS file for styling 
 
 function AboutMe() {
   return (
-    <div>
+    <div className='about-container'>
       <h2>About Me</h2>
       <p>I am a passionate developer who loves coding and building awesome projects.</p>
       <p>This is just a simple example of an About Me page.</p>
