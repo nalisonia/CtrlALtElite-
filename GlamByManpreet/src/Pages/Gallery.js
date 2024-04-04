32e008de57433d78d2cffb5e88adc9c7dfbee46d
Ruben <ruben000_2@live.com>
2024-04-03 21:08:17 -0700

    Worked more on the nav bar added photos and put them in their own section. Made the nav bar stay at top of screen

--- GlamByManpreet/src/Pages/Gallery.js
+++ GlamByManpreet/src/Pages/Gallery.js
@@ -3,7 +3,7 @@ import '../Styles/AboutMe.css'; // Import CSS file for styling
 
 function Gallery() {
   return (
-    <div className='about-container'>
+    <div className='gallery-container'>
       <h2>Gallery</h2>
       <p>I am a passionate developer who loves coding and building awesome projects.</p>
       <p>This is just a simple example of an About Me page.</p>
