f787897d7c7c4d9cc4ea38ef32a90be58b89215a
DiyaSoneji <diya.soneji@gmail.com>
2024-04-07 12:57:48 -0700

    #Scrum 62 & 79
    Uploaded a placeholder for the photos on the gallery page. Also added images 3 & 4 to the home page.

--- GlamByManpreet/src/Pages/Home.js
+++ GlamByManpreet/src/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import model1 from '../assets/images/homepage_image1.png';
 import model2 from '../assets/images/homepage_image2.png';
+import model3 from '../assets/images/homepage_image3.png';
+import model4 from '../assets/images/homepage_image4.png';
+
 import '../Styles/Home.css'; // Import CSS file for styling
 
 function HomePage() {
@@ -9,6 +12,9 @@ function HomePage() {
             <div className='picture-container'>
                 <img src={model1} alt="model1" className="model-image" />
                 <img src={model2} alt="model2" className="model-image" />
+                <img src={model3} alt="model3" className="model-image" />
+                <img src={model4} alt="model4" className="model-image" />
+
             </div>
         </div>
     );
