a8702047c0a9b62929194a91be2a57caa9b1a740
Nickdoctor <ngugliemo@csus.edu>
2024-04-15 23:11:06 -0700

    Task # 96 - Login/Register v 1.0

--- GlamByManpreet/src/Pages/LogIn.js
+++ GlamByManpreet/src/Pages/LogIn.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import '../Styles/LogIn.css'; // Import CSS file for styling 
 
-
 function LogIn() {
   return (
     <div className='LogIn-container'>
-      
-      <h2>LogIn Page</h2>
-      <p>This is the log in page</p>
-      <p>NG</p>
+      <div className='LogIn-title'>
+        <h1>Member Login</h1>
+      </div>
+      <div className='LogIn-form'>
+        <form>
+          <div>
+            <label htmlFor='email'> Email:*</label>
+            <div></div>
+            <input type ='email' id= 'email' name='email'/>
+          </div>
+          <div>
+            <label htmlFor='password'> Password:*</label>
+            <div></div>
+            <input type ='password' id= 'password' name='password'/>
+          </div>
+          <button type ='submit'> Log In</button>
+          <div></div>
+          <button type ='submit'> Forgot Password</button>
+        </form>
+      </div>
     </div>
   );
 }
--- GlamByManpreet/src/Pages/Register.js
+++ GlamByManpreet/src/Pages/Register.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import '../Styles/Register.css'; // Import CSS file for styling 
 
-
 function Register() {
   return (
     <div className='Register-container'>
-      
-      <h2>Register Page</h2>
-      <p>This is the Register page</p>
-      <p>NG</p>
+      <div className='Register-title'>
+        <h1>Register</h1>
+      </div>
+      <div className='Register-form'>
+        <form>
+          <div>
+            <label htmlFor='firstName'> First Name:*</label>
+            <div></div>
+            <input type ='text' id= 'firstName' name='firstName'/>
+          </div>
+          <div>
+            <label htmlFor='lastName'> Last Name:*</label>
+            <div></div>
+            <input type ='text' id= 'lastName' name='lastName'/>
+          </div>
+          <div>
+            <label htmlFor='email'> Email:*</label>
+            <div></div>
+            <input type ='email' id= 'email' name='email'/>
+          </div>
+          <div>
+            <label htmlFor='password'> Password:*</label>
+            <div></div>
+            <input type ='password' id= 'password' name='password'/>
+          </div>
+          <div className="checkbox-container">
+            <input type='checkbox' id='agreeTerms' name='agreeTerms'/>
+            <label htmlFor='agreeTerms'>I agree to the <a href="/terms">Terms of Service</a> and <a href="/privacy">Privacy Policy</a>.</label>
+          </div>
+          <button type ='submit'> Register</button>
+        </form>
+      </div>
     </div>
   );
 }
