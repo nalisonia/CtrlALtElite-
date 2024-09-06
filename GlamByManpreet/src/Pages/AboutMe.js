631feee63783a23dc2dc5e8da9ca5bb8c7e74db9
Nickdoctor <ngugliemo@csus.edu>
2024-09-05 20:47:57 -0700

    #64 Work on about me section (Draft)

--- GlamByManpreet/src/Pages/AboutMe.js
+++ GlamByManpreet/src/Pages/AboutMe.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import pic1 from '../assets/images/about_me.png';
 import '../Styles/AboutMe.css'; // Import CSS file for styling 
-
 
 function AboutMe() {
   return (
     <div className='about-container'>
-      
-      <h2>About Me</h2>
-      <p>I am a passionate developer who loves coding and building awesome projects.</p>
-      <p>This is just a simple example of an About Me page.</p>
-      
+      <div className='text-box'>
+        <h2>About Me</h2>
+        <p>Welcome to my world of beauty in the heart of Sacramento!
+           As a makeup artist specializing in Modern South Asian Glam,
+           I'm dedicated to crafting breathtaking bridal hair and makeup
+           looks that merge tradition with contemporary flair. I not only
+           transform faces but also empower aspiring artists through education.
+           Join me on this beauty journey, where every brushstroke tells a story
+           of elegance and empowerment.
+            -Manpreet Chahal.</p> 
+      </div>
+      <div className='picture-container'>
+        <img src={pic1} alt="pic1" className="model-image" />
+      </div>
     </div>
   );
 }
 
 export default AboutMe;
-
