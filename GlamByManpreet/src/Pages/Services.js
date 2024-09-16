bf6be9bf187bc0aab6f388397e5036855966794a
unknown <curtis2428@gmail.com>
2024-09-15 20:29:10 -0700

    Revert "#63 Services page"
    This reverts commit a820ae560df9a9c7a40635891dfab4f12615e0a6.

--- GlamByManpreet/src/Pages/Services.js
+++ GlamByManpreet/src/Pages/Services.js
@@ -1,39 +1,69 @@
 import React from 'react';
-import '../Styles/Services.css'; 
-import makeupIcon from '../assets/images/makeup-icon.png'; 
-import masterclassIcon from '../assets/images/masterclass-icon.png'; 
-import skincareIcon from '../assets/images/skincare-icon.png'; // Correct path for the skincare icon
-
+import '../Styles/Services.css'; // Import CSS file for styling 
 
 function Services() {
   return (
     <div className='services-container'>
-      <h2 className='header-text'>Services</h2>
-      
-      <div className="service-box">
-        <img src={makeupIcon} alt="Makeup Icon" className="service-icon" />
-        <h3>MAKEUP</h3>
-        <p>Heavy<br />Natural<br />Bridal<br />Non-bridal</p>
-        <a href="/booking_Inquiry" className="button">Booking Inquiry</a>
-      </div>
-      
-      <div className="service-box">
-        <img src={masterclassIcon} alt="Master Class Icon" className="service-icon" />
-        <h3>MASTER CLASS</h3>
-        <p>Bridal and Non-bridal Lessons<br />Hair<br />Makeup</p>
-        <a href="Register" className="button">Learn More</a>
-      </div>
-    
-      {/* Skincare Box */}
-      <div className="service-box">
-        <img src={skincareIcon} alt="Skin Care Icon" className="service-icon" />
-        <h3>SKINCARE</h3>
-        <p>Coming soon</p>
-        <a href="Register" className="button">Register and get exclusive insights here!</a>
-      </div>
+      <h2>Services Page</h2>
+      <p>I am a passionate developer who loves coding and building awesome projects.</p>
+      <p>We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+And if you ask me how I'm feeling
+Don't tell me you're too blind to see
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give, never gonna give
+(Give you up)
+(Ooh) Never gonna give, never gonna give
+(Give you up)
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry</p>
     </div>
   );
 }
 
 export default Services;
-
