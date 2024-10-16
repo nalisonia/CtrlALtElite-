6f53d8c0282fd71e5920f07948d8a009cbcb6a03
nalisonia <shonali2002@gmail.com>
2024-10-15 19:48:35 -0700

    #161 and #157

--- GlamByManpreet/src/Pages/ContactMe.js
+++ GlamByManpreet/src/Pages/ContactMe.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import '../Styles/ContactMe.css'; // Import CSS file for styling 
+import '../Styles/ContactMe.css';  // Adjust the path based on your file structure
 
 function ContactMe() {
-  return (
-    <div className='contactme-container'>
-      <h2>ContactMe Page</h2>
-      <p>I am a passionate developer who loves coding and building awesome projects.</p>
-      <p>This is just a simple example of an About Me page.</p>
-    </div>
-  );
+    return (
+        <div className='contactme-container'>
+            <h2>Contact Me Page</h2>
+            <p>Hello! I'd love to hear from you—get in touch with me.</p>
+
+            <div className="contact-details">
+                <p>Email: 
+                    <a href="mailto:youremail@example.com" className="email-link">
+                        glambymanpreetk@gmail.com
+                    </a>
+                </p>
+                <p>Business Phone Number: <span>(123) 456-7890</span></p>
+                <p>Instagram: <span>@glambymanpreet</span></p>
+            </div>
+
+            <div className="disclaimer">
+                <p>DISCLAIMER: Takes 1 - 3 business days for response back.</p>
+            </div>
+        </div>
+    );
 }
 
 export default ContactMe;
