853dc559ce7e616e791f1a827c1e7a06db055dce
hashtags2023 <146301949+hashtags2023@users.noreply.github.com>
2024-11-06 19:17:47 -0800

    Apply local styling updates

--- GlamByManpreet/src/Pages/ContactMe.js
+++ GlamByManpreet/src/Pages/ContactMe.js
@@ -1,12 +1,11 @@
+
 import React from 'react';
 import '../Styles/ContactMe.css';  // Adjust the path based on your file structure
-
 function ContactMe() {
     return (
         <div className='contactme-container'>
-            <h2>Contact Me Page</h2>
+            <h2>CONTACT ME</h2> {/* Updated heading */}
             <p>Hello! I'd love to hear from you—get in touch with me.</p>
-
             <div className="contact-details">
                 <p>Email: 
                     <a href="mailto:youremail@example.com" className="email-link">
@@ -19,12 +18,10 @@ function ContactMe() {
                     rel="noopener noreferrer">@glambymanpreet</a>
                 </p>
             </div>
-
             <div className="disclaimer">
                 <p>DISCLAIMER: Takes 1 - 3 business days for response back.</p>
             </div>
         </div>
     );
 }
-
 export default ContactMe;
