7987442d70ffcf1e7f4585f7b41ec82423647ec9
Nickdoctor <ngugliemo@csus.edu>
2024-11-18 20:49:29 -0800

    Made Contact me Dynamic for future changes

--- GlamByManpreet/src/Pages/ContactMe.js
+++ GlamByManpreet/src/Pages/ContactMe.js
@@ -2,25 +2,30 @@ import React from 'react';
 import '../Styles/ContactMe.css';  // Adjust the path based on your file structure
 
 function ContactMe() {
+
+    const clientEmailLink = "mailto:glambymanpreetk@gmail.com" // Change if client email or support email changes
+    const clientEmailInText = "glambymanpreetk@gmail.com" // Change if client email or support email changes
+    const clientInstaLink =  "https://www.instagram.com/glambymanpreet?igsh=MzRlODBiNWFlZA=="
+    const clientInstaLinkInText =  "@glambymanpreet"
     return (
         <div className="contactme-container">
             <h2>CONTACT ME</h2>
             <p className="intro-text">Hello! I'd love to hear from you—get in touch with me.</p>
             <div className="contact-details">
                 <p>Email: 
-                    <a href="mailto:glambymanpreetk@gmail.com" className="email-link">
-                        glambymanpreetk@gmail.com
+                    <a href={clientEmailLink} className="email-link">
+                        {clientEmailInText}
                     </a>
                 </p>
                 <p>If you don't hear back from me, please register to see updates on your booking.</p>
                 <p>Instagram: 
                     <a 
-                        href="https://www.instagram.com/glambymanpreet?igsh=MzRlODBiNWFlZA==" 
+                        href={clientInstaLink} 
                         target="_blank" 
                         rel="noopener noreferrer"
                         className="instagram-link"
                     >
-                        @glambymanpreet
+                        {clientInstaLinkInText}
                     </a>
                 </p>
             </div>
