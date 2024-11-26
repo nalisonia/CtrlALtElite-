093a6ac9da91e6e74342677771137dff031d891a
Nickdoctor <ngugliemo@csus.edu>
2024-11-25 21:46:28 -0800

    Testing admin fix?

--- GlamByManpreet/src/Components/navbar.js
+++ GlamByManpreet/src/Components/navbar.js
@@ -136,8 +136,7 @@ function DrawerAppBar(props) {
       <List>
         
         {/*Itterates over the array which holds the names of the pages*/}
-        {navItems.map((item) => (
-          
+        {(session ? [...baseNavItems, isAdmin ? "ADMIN" : "PROFILE"] : baseNavItems).map((item) => (
           //creates the list of About Me, Services, Booking Inquiry ...etc
           <ListItem key={item} disablePadding>
             {/*styling for each button inside the list*/}
@@ -161,7 +160,8 @@ function DrawerAppBar(props) {
               </Link>
             </ListItemButton>
           </ListItem>
-        ))}
+        )
+        )}
       </List>
       
 
